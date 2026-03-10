bae79350e3d1ed929864ba4ff9761d1d1b043d83
Sergiu2404 <sergiu_goian@yahoo.com>
2026-03-10 18:15:03 +0200

    initial setup

---
+++ auction.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace auction.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ auction.Worker/Program.cs
@@ -0,0 +1,7 @@
+using auction.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
